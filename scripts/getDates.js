d7f0776b185c78c607ae35570e1c39a9b7c22afa
andrek4z <andre.fernandez.98@hotmail.com>
2023-09-17 02:20:47 -0500

    first commit

---
+++ scripts/getDates.js
@@ -0,0 +1,9 @@
+const copyrightYear = new Date().getFullYear();
+
+const lastModifiedDate = document.lastModified;
+
+const copyrightParagraph = document.querySelector("footer p:first-child");
+copyrightParagraph.textContent = `© ${copyrightYear} Favio Andre Fernandez Mori. Peru.`;
+
+const lastModifiedParagraph = document.querySelector("footer p:last-child");
+lastModifiedParagraph.textContent = `Last modified on ${lastModifiedDate}`;
