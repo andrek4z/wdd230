3cd889796ad2ae40f69e1a1767f8ecf1bdad05d6
andrek4z <andre.fernandez.98@hotmail.com>
2023-09-24 02:23:02 -0500

    Done

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -2,7 +2,7 @@ const copyrightYear = new Date().getFullYear();
 const lastModifiedDate = document.lastModified;
 
 const copyrightParagraph = document.querySelector("footer p:first-child");
-copyrightParagraph.textContent = `COPYRIGHT © ${copyrightYear}`;
+copyrightParagraph.textContent = `COPYRIGHT © ${copyrightYear} This is a project for WWD 230 by Favio Andre Fernandez Mori`;
 
 const lastModifiedParagraph = document.querySelector("footer p:last-child");
 lastModifiedParagraph.textContent = `Last modification: ${lastModifiedDate}`;
