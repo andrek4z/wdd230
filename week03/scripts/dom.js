a9c8cd57cf79dbd7322ec97239bc7b28be8a9957
andrek4z <andre.fernandez.98@hotmail.com>
2023-09-28 01:19:56 -0500

    Done

--- week03/scripts/dom.js
+++ week03/scripts/dom.js
@@ -35,7 +35,6 @@ function displayList(item)
       deleteChapter(li.textContent);
       input.focus();
     });
-    console.log("I like to copy code instead of typing it out myself and trying to understand it.");
 }
 
 function setChapterList() 
