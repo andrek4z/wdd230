cc9047d82092c10158edc962e2f5503bb25e11e6
andrek4z <andre.fernandez.98@hotmail.com>
2023-09-30 00:56:28 -0500

    Done

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -2,7 +2,7 @@ const copyrightYear = new Date().getFullYear();
 const lastModifiedDate = document.lastModified;
 
 const copyrightParagraph = document.querySelector("footer p:first-child");
-copyrightParagraph.textContent = `COPYRIGHT © ${copyrightYear} This is a project for WWD 230 by Favio Andre Fernandez Mori`;
+copyrightParagraph.textContent = `©${copyrightYear} Trujillo Chamber`;
 
 const lastModifiedParagraph = document.querySelector("footer p:last-child");
-lastModifiedParagraph.textContent = `Last modification: ${lastModifiedDate}`;
+lastModifiedParagraph.textContent = `Last Modification: ${lastModifiedDate}`;
