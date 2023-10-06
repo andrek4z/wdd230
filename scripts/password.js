d2c55407e966fb3e495d6e52c3a67522e836fda8
andrek4z <andre.fernandez.98@hotmail.com>
2023-10-06 02:52:10 -0500

    Done

---
+++ scripts/password.js
@@ -0,0 +1,24 @@
+
+const p1 = document.querySelector("#password");
+const p2 = document.querySelector("#rpassword");
+const message = document.querySelector("#formmessage");
+
+p2.addEventListener("focusout", checkSame);
+
+function checkSame()
+{
+    if(p1.value !== p2.value)
+    {
+        message.textContent = "Passwords DO NOT match";
+        message.style.visibility = "show";
+        p1.value = "";
+        p2.value = "";
+        p1.focus();
+    }
+    else
+    {
+        message.style.display = "none";
+        p1.style.backgroundColor = "rgb(179, 222, 179)";
+        p2.style.backgroundColor = "rgb(179, 222, 179)";
+    }
+}
