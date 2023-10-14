172ccc2e997e06f740b2487742a74b57b7ec0eba
andrek4z <andre.fernandez.98@hotmail.com>
2023-10-13 21:50:15 -0500

    Done

--- scripts/links.js
+++ scripts/links.js
@@ -1,11 +1,43 @@
 const baseURL = "https://andrek4z.github.io/wdd230/";
 const linksURL = "https://andrek4z.github.io/wdd230/data/links.json";
+const linkWeek = document.querySelector("#linkWeeks")
 
 async function getLinks()
 {
     const response = await fetch(linksURL);
     const data = await response.json();
     console.log(data);
+    displayLinks(data.weeks);
+}
+
+const displayLinks = (weeks) =>
+{
+    let ulist = document.createElement("ul");
+
+    weeks.forEach((week) =>
+    {
+        let list = document.createElement("li");
+
+        list.textContent = `${week.week}: `
+        
+        week.links.forEach((link,index) =>
+        {
+            let a = document.createElement("a");
+            
+            a.setAttribute("href", link.url);
+            a.setAttribute("target", "_blank");
+            a.textContent = link.title;
+            list.append(a);
+            if(index !== week.links.length-1)
+            {
+                list.innerHTML += " | ";
+            }
+        });
+
+        ulist.append(list);
+    });
+
+    linkWeek.append(ulist);
 }
 
 getLinks();
