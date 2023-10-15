5fbcc1ebbd8f67f5424d0066665fb06463aa0649
andrek4z <andre.fernandez.98@hotmail.com>
2023-10-14 19:15:30 -0500

    Done

---
+++ chamber/scripts/members.js
@@ -0,0 +1,45 @@
+const baseURL = "https://andrek4z.github.io/wdd230/chamber/";
+const membersURL = "https://andrek4z.github.io/wdd230/chamber/data/members.json";
+const gridMembers = document.querySelector("#gridMe");
+const listMembers = document.querySelector("#listMe");
+
+async function getMembers()
+{
+    const response = await fetch(membersURL);
+    const data = await response.json();
+    console.log(data);
+    displayMembersGrid(data.members);
+}
+
+const displayMembersGrid = (members) =>
+{
+    members.forEach((member) =>
+    {
+        let card = document.createElement("section");
+        let icon = document.createElement("img");
+        let address = document.createElement("li");
+        let number = document.createElement("li");
+        let websiteURL = document.createElement("li");
+
+        icon.setAttribute("src", member.imagenURLs);
+        icon.setAttribute('alt', `Icon of ${member.name}`);
+        icon.setAttribute('loading', 'lazy');
+        icon.setAttribute('width', '200');
+        icon.setAttribute('height', '180');
+
+        address.textContent = member.address;
+        number.textContent = member.phoneNumber;
+
+        websiteURL.setAttribute("href", member.websiteURLs);
+        websiteURL.textContent = member.websiteURLs;
+
+        card.appendChild(icon);
+        card.appendChild(address);
+        card.appendChild(number);
+        card.appendChild(websiteURL);
+
+        gridMembers.appendChild(card);
+    });
+}
+
+getMembers()
