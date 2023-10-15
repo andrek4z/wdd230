cf4937b617e3a55d614b1dfdaafeb9e87798d662
andrek4z <andre.fernandez.98@hotmail.com>
2023-10-14 19:41:50 -0500

    Done

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -9,6 +9,7 @@ async function getMembers()
     const data = await response.json();
     console.log(data);
     displayMembersGrid(data.members);
+    displayMembersList(data.members);
 }
 
 const displayMembersGrid = (members) =>
@@ -42,4 +43,46 @@ const displayMembersGrid = (members) =>
     });
 }
 
+const displayMembersList = (members) =>
+{
+    members.forEach((member) =>
+    {
+        let card = document.createElement("section");
+        let name = documente.createElement("li");
+        let address = document.createElement("li");
+        let number = document.createElement("li");
+        let websiteURL = document.createElement("li");
+        
+        name.textContent = member.name;
+        address.textContent = member.address;
+        number.textContent = member.phoneNumber;
+
+        websiteURL.setAttribute("href", member.websiteURLs);
+        websiteURL.textContent = member.websiteURLs;
+
+        card.appendChild(name);
+        card.appendChild(address);
+        card.appendChild(number);
+        card.appendChild(websiteURL);
+
+        listMembers.appendChild(card);
+    });
+}
+
 getMembers()
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+gridbutton.addEventListener("click", () => {
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList);
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
