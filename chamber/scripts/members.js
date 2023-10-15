c339d7d4bdfaf84bd861e080682ae07c6716882d
andrek4z <andre.fernandez.98@hotmail.com>
2023-10-14 20:16:14 -0500

    Done

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -9,7 +9,6 @@ async function getMembers()
     const data = await response.json();
     console.log(data);
     displayMembersGrid(data.members);
-    displayMembersList(data.members);
 }
 
 const displayMembersGrid = (members) =>
@@ -18,6 +17,7 @@ const displayMembersGrid = (members) =>
     {
         let card = document.createElement("section");
         let icon = document.createElement("img");
+        let name = document.createElement("li");
         let address = document.createElement("li");
         let number = document.createElement("li");
         let websiteURL = document.createElement("li");
@@ -28,6 +28,7 @@ const displayMembersGrid = (members) =>
         icon.setAttribute('width', '200');
         icon.setAttribute('height', '180');
 
+        name.textContent = member.name;
         address.textContent = member.address;
         number.textContent = member.phoneNumber;
 
@@ -35,6 +36,7 @@ const displayMembersGrid = (members) =>
         websiteURL.textContent = member.websiteURLs;
 
         card.appendChild(icon);
+        card.appendChild(name);
         card.appendChild(address);
         card.appendChild(number);
         card.appendChild(websiteURL);
@@ -43,46 +45,4 @@ const displayMembersGrid = (members) =>
     });
 }
 
-const displayMembersList = (members) =>
-{
-    members.forEach((member) =>
-    {
-        let card = document.createElement("section");
-        let name = documente.createElement("li");
-        let address = document.createElement("li");
-        let number = document.createElement("li");
-        let websiteURL = document.createElement("li");
-        
-        name.textContent = member.name;
-        address.textContent = member.address;
-        number.textContent = member.phoneNumber;
-
-        websiteURL.setAttribute("href", member.websiteURLs);
-        websiteURL.textContent = member.websiteURLs;
-
-        card.appendChild(name);
-        card.appendChild(address);
-        card.appendChild(number);
-        card.appendChild(websiteURL);
-
-        listMembers.appendChild(card);
-    });
-}
-
 getMembers()
-
-const gridbutton = document.querySelector("#grid");
-const listbutton = document.querySelector("#list");
-const display = document.querySelector("article");
-
-gridbutton.addEventListener("click", () => {
-	display.classList.add("grid");
-	display.classList.remove("list");
-});
-
-listbutton.addEventListener("click", showList);
-
-function showList() {
-	display.classList.add("list");
-	display.classList.remove("grid");
-}
