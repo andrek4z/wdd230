ad1bc18c08acdc10ef0f341d2556035e796f402f
andrek4z <andre.fernandez.98@hotmail.com>
2023-10-24 20:28:56 -0500

    Done

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,7 +1,9 @@
 const currentTemp = document.querySelector("#current-temp");
 const weatherIcon = document.querySelector("#weather-icon");
 const captionDesc = document.querySelector("figcaption");
+const forecast = document.querySelector("#forecast");
 const url = "https://api.openweathermap.org/data/2.5/weather?lat=-8.115733925302925&lon=-79.02470844951075&units=imperial&appid=a1d6f7e3010dc4c7ec86d23b888050b6";
+const urlForecast = "https://api.openweathermap.org/data/2.5/forecast?lat=-8.115733925302925&lon=-79.02470844951075&units=imperial&cnt=3&appid=a1d6f7e3010dc4c7ec86d23b888050b6";
 
 async function apiFetch()
 {
@@ -35,4 +37,39 @@ function displayResults(data)
     captionDesc.textContent = `${desc}`;
 }
 
+async function apiForecast()
+{
+    try
+    {
+        const response = await fetch(urlForecast);
+        if(response.ok)
+        {
+            const data = await response.json();
+            displayForecast(data);
+            console.log(data);
+        }
+        else
+        {
+            throw Error(await response.text());
+        }
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+}
+
+function displayForecast(data)
+{
+    data.list.forEach((item) => 
+    {
+        const day = document.createElement('p');
+    
+        day.textContent = `${item.dt_txt} - ${item.main.temp} - ${item.weather[0].main}`;
+    
+        forecast.appendChild(day);
+    });
+}
+
+apiForecast();
 apiFetch();
